2321c72c7ec4a8a58b8d37d2c4c4bc8e618c7673
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-02 19:57:34 +0100

    no message

--- scripts/mygallery.js
+++ scripts/mygallery.js
@@ -2,7 +2,7 @@ hexo.extend.tag.register('mygallery', function (args) {
 	var galleryName = args[0] || '',htmlTxt='';
 	var fs = require('hexo-fs'), path = require('path');
 	fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).forEach(function(curr){
-		htmlTxt+='<a href="/images/'+galleryName+'/'+curr+'"><img class=gallth src="/images/'+galleryName+'/'+curr+'"></a>';
+		htmlTxt+='<a href="/images/'+galleryName+'/'+curr+'"><img class="gallth" src="/images/'+galleryName+'/'+curr+'"></a>';
 	});
 	
 	
