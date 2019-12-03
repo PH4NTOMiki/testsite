913551a9fb04aa6f975fde4ee4c526f63f001830
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-03 18:08:22 +0100

    gallery thumbs

--- scripts/mygallery.js
+++ scripts/mygallery.js
@@ -3,7 +3,7 @@ hexo.extend.tag.register('mygallery', function (args) {
 	var fs = require('hexo-fs'), path = require('path');
 	fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(function(curr){return curr.indexOf('-thumb')<0}).forEach(function(curr){
 		let thumb=curr.split('.');thumb[thumb.length-2]+='-thumb';
-		htmlTxt+='<a data-fancybox-href="/images/'+galleryName+'/'+curr+'" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
+		htmlTxt+='<a class="fancybox" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
 	});
 	
 	
