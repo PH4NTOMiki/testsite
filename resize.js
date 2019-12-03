ddd20a0e9307c1f672e363fedb4d606f615d0d9a
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-03 18:21:55 +0100

    Update resize.js

--- resize.js
+++ resize.js
@@ -10,16 +10,22 @@ const walkSync = (dir, filelist = []) => {
   });
 return filelist;
 }
-
-walkSync(path.join(__dirname,'source','images')).forEach(function(img){
+//counter=0;
+var arr=walkSync(path.join(__dirname,'source','images'));
+arr.forEach(function(img){
+	let n=img.split('.');
+	n[n.length-2]+='-thumb';
+	n=n.join('.');
+	if(img.indexOf('-thumb')<0 && arr.indexOf(n)<0){
 	sizeOf(img, function (err, dimensions){
 		//console.log(dimensions.width,dimensions.height)
 		let options = dimensions.width>=dimensions.height ? {width:200,height:160} : {width:200,height:269};
 		//console.log(options);
 		imageThumbnail(img,options).then(thumbnail=>{
-			let n=img.split('.');
-			n[n.length-2]+='-thumb';
-			fs.writeFileSync(n.join('.'),thumbnail);
+			fs.writeFileSync(n,thumbnail);
 		})
 	})
+	//counter++;
+	}
 });
+//console.log(counter);
