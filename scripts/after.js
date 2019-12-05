3d6c9a2fb4ae127da7aff7d870aff6c8aa20cb25
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-05 19:44:32 +0100

    Update after.js

--- scripts/after.js
+++ scripts/after.js
@@ -1,10 +1,12 @@
 hexo.extend.filter.register('before_exit', function(){
 	var fs = require('hexo-fs'), path = require('path');
 	var arr = [];
+	if(fs.existsSync(path.join(__dirname,'..','public','feed.json'))){
 	JSON.parse(fs.readFileSync(path.join(__dirname,'..','public','feed.json'))).items.forEach(function(item){
 		let curr={title:item.title,uri:item.id,description:''};
 		if(item.id.indexOf('galerija/')<0)curr.description=item.content_html;
 		arr.push(curr);
 	});
 	fs.writeFileSync(path.join(__dirname,'..','public','json-feed.json'),JSON.stringify(arr));
+	}
 });
