39d4c0ebacff40aa819e5fe4921d021503dd941e
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-05 20:12:37 +0100

    Update after.js

--- scripts/after.js
+++ scripts/after.js
@@ -1,10 +1,10 @@
 hexo.extend.filter.register('before_exit', function(){
 	var fs = require('hexo-fs'), path = require('path');
-	var arr = [];
+	var arr = [], url = hexo.config.url + '/';
 	if(fs.existsSync(path.join(__dirname,'..','public','feed.json'))){
 	JSON.parse(fs.readFileSync(path.join(__dirname,'..','public','feed.json'))).items.forEach(function(item){
-		let curr={title:item.title,uri:item.id,description:''};
-		if(item.id.indexOf('galerija/')<0)curr.description=item.content_html;
+		let curr={title:item.title,uri:item.id.replace(url,''),content:''};
+		if(item.id.indexOf('galerija/')<0)curr.content=item.content_html;
 		arr.push(curr);
 	});
 	fs.writeFileSync(path.join(__dirname,'..','public','json-feed.json'),JSON.stringify(arr));
