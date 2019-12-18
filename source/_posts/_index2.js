abd7326e56163e2911cd16771d54ac4ca384d057
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-18 17:05:51 +0100

    ,

--- source/_posts/_index2.js
+++ source/_posts/_index2.js
@@ -2,7 +2,7 @@ var path = require('path'),
 fs = require('fs');
 
 fs.readdirSync(__dirname).forEach(file=>{
-    if(/.*\.md/.test(file)){
+    if(/.*\.md$/.test(file)){
 		content=fs.readFileSync(path.join(__dirname, file), 'utf8');
 		content=content.replace(/\[(.*)\]\((.*)\)/gi,function(m,a,b){
 			return '<a href="'+b+'">'+a+'</a>';
