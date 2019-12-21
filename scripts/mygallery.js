2b894b09e50420f96e1fb3b3ef5c7cc2ca507ff3
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-21 19:18:54 +0100

    limit gallery entries

--- scripts/mygallery.js
+++ scripts/mygallery.js
@@ -1,8 +1,9 @@
 hexo.extend.tag.register('mygallery', function (args) {
 	var galleryName = args[0] || '',htmlTxt='';
+	var max = args[1] || undefined;
 	var fs = require('hexo-fs'), path = require('path');
 	if(!fs.existsSync(path.join(__dirname,'..','source','images',galleryName))){hexo.log.info('mygallery: '+galleryName+' does not exist!');} else {
-	fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(function(curr){return curr.indexOf('-thumb')<0}).forEach(function(curr){
+	fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(function(curr){return curr.indexOf('-thumb')<0}).slice(0,max).forEach(function(curr){
 		let thumb=curr.split('.');thumb[thumb.length-2]+='-thumb';
 		htmlTxt+='<a class="fancybox" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
 	});}
--- source/_posts/galerija/bozicno-novogodisnja-priredba-2019.md
+++ source/_posts/galerija/bozicno-novogodisnja-priredba-2019.md
@@ -6,4 +6,4 @@ date: 2019-12-19 14:00:00
 tags:
 ---
 
-{% mygallery bozicno-novogodisnja-priredba-2019 %}
+{% mygallery bozicno-novogodisnja-priredba-2019 10 %}
