9ca623b821a16ffd2990b1d4a4700f3b0197ea39
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-22 15:39:11 +0100

    Update mygallery.js

--- scripts/mygallery.js
+++ scripts/mygallery.js
@@ -1,17 +1,19 @@
 hexo.extend.tag.register('mygallery', function (args) {
-	var galleryName = args[0] || '',htmlTxt='';
-	var max = args[1] || undefined;
+	if(!args[0]){return '';}
+	var galleryName = args[0],
+	max = args[1] || undefined;
 	var fs = require('hexo-fs'), path = require('path');
 	if(!fs.existsSync(path.join(__dirname,'..','source','images',galleryName))){hexo.log.info('mygallery: '+galleryName+' does not exist!');} else {
-	fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(function(curr){return curr.indexOf('-thumb')<0}).slice(0,max).forEach(function(curr){
+	return '<center>' + (fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(c=>c.indexOf('-thumb')<0).slice(0,max).map(curr=>{
 		let thumb=curr.split('.');thumb[thumb.length-2]+='-thumb';
-		htmlTxt+='<a class="fancybox" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
-	});}
+		return '<a class="fancybox" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
+	})||'') + '</center>';
+	}
 	
 	
 	
 	
 	
 	
-	return htmlTxt;
+	
 });
