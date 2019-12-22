be32271bba418522498e95034471d340c53b76d2
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-22 16:24:23 +0100

    update

--- number-index-images.js
+++ number-index-images.js
@@ -3,8 +3,8 @@ var fs = require('fs'), path = require('path'),
 folders=fs.readdirSync(path.join(__dirname,'source','images'),{withFileTypes:true}).filter(dirent=>dirent.isDirectory()).map(dirent=>dirent.name);
 // console.log(JSON.stringify(fs.readdirSync(path.join(__dirname,'source','images'))));
 folders.forEach(name=>{
-	let relPath=path.join(__dirname,'source','images',name), i=1;
-	fs.readdirSync(relPath).forEach(img=>{
-		/\d{1,10}\.(jpg|png)/i.test(img)||fs.renameSync(path.join(relPath, img), path.join(relPath, nf(i++,3)+'.jpg'));
+	let relPath=path.join(__dirname,'source','images',name);
+	fs.readdirSync(relPath).forEach((img, i)=>{
+		/\d{1,10}\.(jpg|png)/i.test(img)||fs.renameSync(path.join(relPath, img), path.join(relPath, nf(i+1,3)+'.jpg'));
 	});
 });
--- source/_posts/galerija/bozicno-novogodisnja-priredba-2019.md
+++ source/_posts/galerija/bozicno-novogodisnja-priredba-2019.md
@@ -6,6 +6,4 @@ date: 2019-12-19 14:00:00
 tags:
 ---
 
-{% mygallery bozicno-novogodisnja-priredba-2019 12 %}
-<br>
-<a href="/galerija/bozicno-novogodisnja-priredba-19-12-2019">Sve fotografije</a>
+{% mygallery bozicno-novogodisnja-priredba-2019 %}
