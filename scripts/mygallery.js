43886a4c44bed69ea7907a4595b5674abad69473
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-22 16:16:23 +0100

    Update mygallery.js

--- scripts/mygallery.js
+++ scripts/mygallery.js
@@ -4,7 +4,7 @@ hexo.extend.tag.register('mygallery', function (args) {
 	max = args[1] || undefined;
 	var fs = require('hexo-fs'), path = require('path');
 	if(!fs.existsSync(path.join(__dirname,'..','source','images',galleryName))){hexo.log.info('mygallery: '+galleryName+' does not exist!');} else {
-	return '<center>' + (fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(c=>{return !c.includes('-thumb')}).slice(0,max).map(curr=>{
+	return '<center>' + (fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(c=>c.indexOf('-thumb')<0).slice(0,max).map(curr=>{
 		let thumb=curr.split('.');thumb[thumb.length-2]+='-thumb';
 		return '<a class="fancybox" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
 	}).join('')||'') + '</center>';
