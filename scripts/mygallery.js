f7d046459a1255f6e92c732256bd24f6024a31de
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2019-12-22 15:41:28 +0100

    center gallery

--- scripts/mygallery.js
+++ scripts/mygallery.js
@@ -7,7 +7,7 @@ hexo.extend.tag.register('mygallery', function (args) {
 	return '<center>' + (fs.listDirSync(path.join(__dirname,'..','source','images',galleryName)).filter(c=>c.indexOf('-thumb')<0).slice(0,max).map(curr=>{
 		let thumb=curr.split('.');thumb[thumb.length-2]+='-thumb';
 		return '<a class="fancybox" href="/images/'+galleryName+'/'+curr+'"><img class="gallth" width="200px" height="160px" src="/images/'+galleryName+'/'+thumb.join('.')+'"></a>';
-	})||'') + '</center>';
+	}).join('')||'') + '</center>';
 	}
 	
 	
