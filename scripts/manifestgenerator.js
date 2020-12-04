781c4a4725f000731b2c59276b5b27f611d571e2
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-12-04 22:55:29 +0100

    manifest generator added and config updates(added keywords, timezone and some other stuff)

---
+++ scripts/manifestgenerator.js
@@ -0,0 +1,27 @@
+const imageSize = require('image-size'), imageWidth = e => imageSize(e).width;
+
+hexo.extend.generator.register('manifest', manifestGenerator);
+
+function manifestGenerator(site) {
+	var config = hexo.config, mConfig = config.hasOwnProperty('manifest') ? config.manifest : {};
+	if(!Array.isArray(mConfig.icons) || mConfig.icons.length < 2)return;
+	var manifest = {
+		name: mConfig.name || (config.title || 'Website'),
+		short_name: mConfig.short_name || '',
+		theme_color: mConfig.theme_color || '#898e8c',
+		background_color: mConfig.background_color || '#ffffff',
+		display: mConfig.display || 'standalone',
+		start_url: mConfig.start_url || (config.root || '/'),
+		icons: mConfig.icons.map(icon => {
+			let width, matches = /-(\d{1,5})(px)?((x|-)?\d{1,5})?/i.exec(icon);
+			width = matches && matches[1] ? matches[1] : imageWidth('../source/' + icon);
+			
+			return {src: icon, sizes: `${width}x${width}`, type: 'image/png'};
+		}),
+		splash_pages: mConfig.splash_pages || null
+	};
+	
+	if(!manifest.short_name)delete manifest.short_name;
+	
+	return {path: 'manifest.json', data: JSON.stringify(manifest, null, 4)};
+}
