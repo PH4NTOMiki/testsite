5a86e9fddea87fbaead5393a3eabed156da3c685
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-12-08 21:57:15 +0100

    script.js: Array.map polyfill, using native Array.map for Array manipulations

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -1,3 +1,4 @@
+Array.prototype.map||(Array.prototype.map=function(r){var t,n,o;if(null==this)throw new TypeError("this is null or not defined");var e=Object(this),i=e.length>>>0;if("function"!=typeof r)throw new TypeError(r+" is not a function");for(arguments.length>1&&(t=arguments[1]),n=new Array(i),o=0;o<i;){var a,p;o in e&&(a=e[o],p=r.call(t,a,o,e),n[o]=p),o++}return n});
 (function($){window.galleryMaker = galleryMaker;
   // Search
   var $searchWrap = $('#search-form-wrap'),
@@ -106,7 +107,7 @@
     if($center.length){/*var $this = $(this);*/
       function loadImages(){
 	  var imgs = $center.attr('data-imgs').split(','), galleryName = imgs[0];
-      $center.append($.map(imgs.slice(1),function(curr){
+      $center.append(imgs.slice(1).map(function(curr){
 		  var thumb=curr.split('.');thumb[thumb.length-2]+='-thumb';
 		  return '<a class="fancybox" href="<%-c.cdn%><%- c.root && c.root.length>1 ? c.root : c.startslash %>images/'+galleryName+'/'+curr+'" rel="article'+i+'"><img class="gallth" width="200px" height="160px" src="<%-c.cdn%><%- c.root && c.root.length>1 ? c.root : c.startslash %>images/'+galleryName+'/'+thumb.join('.')+'" alt="'/*+curr.split('.').slice(0,-1).join('.')*/+'"></a>';
 	  }).join(''));
