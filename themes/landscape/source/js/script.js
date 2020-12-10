e60b2c0ba32d32de537c17a47d9fb9975742bf6e
Mihael Mutić <35368241+PH4NTOMiki@users.noreply.github.com>
2020-12-10 22:08:36 +0100

    Update script.js

--- themes/landscape/source/js/script.js
+++ themes/landscape/source/js/script.js
@@ -1,5 +1,6 @@
 Array.prototype.map||(Array.prototype.map=function(r){var t,n,o;if(null==this)throw new TypeError("this is null or not defined");var e=Object(this),i=e.length>>>0;if("function"!=typeof r)throw new TypeError(r+" is not a function");for(arguments.length>1&&(t=arguments[1]),n=new Array(i),o=0;o<i;){var a,p;o in e&&(a=e[o],p=r.call(t,a,o,e),n[o]=p),o++}return n});
-(function($){window.checkForUpdates = this.checkForUpdates;window.galleryMaker = this.galleryMaker;window.logVisit = logVisit;
+(function($){<% if (c.ajaxloader){ %>window.checkForUpdates = checkForUpdates;window.galleryMaker = galleryMaker;<% } %>
+window.logVisit = logVisit;
   // Search
   var $searchWrap = $('#search-form-wrap'),
     isSearchAnim = false,
